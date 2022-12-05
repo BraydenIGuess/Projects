ca63dd50631da4c2ddf609b421cb42eb6dfaa2b1
BraydenIGuess <115026785+BraydenIGuess@users.noreply.github.com>
2022-12-05 12:39:36 -0500

    Add files via upload

---
+++ Projects/Hers/index.js
@@ -0,0 +1,13 @@
+aww = null
+clicked = false
+function clickee()
+{
+    console.log("hello world")
+document.getElementById("CHANGEE").innerHTML = 'I LOVE YOUUUUUUU'
+setTimeout(function(){
+    document.getElementById("CHANGEE").innerHTML ="Before u respond with I love you more no u dont"
+},2000)
+    
+    }
+
+  
