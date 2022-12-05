b318613667987662e1f676248f52b12c929aee46
BraydenIGuess <115026785+BraydenIGuess@users.noreply.github.com>
2022-12-05 12:46:50 -0500

    Add files via upload

--- Projects/Hers/index.js
+++ Projects/Hers/index.js
@@ -6,8 +6,14 @@ function clickee()
 document.getElementById("CHANGEE").innerHTML = 'I LOVE YOUUUUUUU'
 setTimeout(function(){
     document.getElementById("CHANGEE").innerHTML ="Before u respond with I love you more no u dont"
-},2000)
+},2000,)
+setTimeout(function(){
+    document.getElementById("CHANGEE").innerHTML ="You probably thought it was gonna be something dirty cause the winky face smhh"
+},4000)
     
+setTimeout(function(){
+    document.getElementById("CHANGEE").innerHTML="NOTHING TO SEE HERE"
+},7000)
     }
 
   
