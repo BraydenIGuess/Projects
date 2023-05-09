4af70f811ca5ea8039c6fa6454c3c5cc81da0a47
BraydenIGuess <115026785+BraydenIGuess@users.noreply.github.com>
2023-05-09 13:54:42 -0400

    Add files via upload

--- BlackJack/index.js
+++ BlackJack/index.js
@@ -1,20 +1,149 @@
-var face_cards = ["jackclubs.png", "jackdiamonds.png", "jackhearts.png", "jackspades.png", "kingclubs", "kingdiamonds.png", "kinghearts.png", "kinghearts.png", "kingspades.png", "queenclubs.png", "queendiamonds.png", "queenhearts.png", "queenspades.png"]
+dealer1 = false
+dealer2 = false
+dealer3 = false
+dealer4 = false
+player1 = false
+player2 = false
+player3 = false
+player4 = false
+ran = false
+var local = false
+player = 1
+var sum;
+var face_cards = ["aceclubs.png","acediamonds.png","acehearts.png","acespades.png","jackclubs.png", "jackdiamonds.png", "jackhearts.png", "jackspades.png", "kingclubs.png", "kingdiamonds.png", "kinghearts.png", "kinghearts.png", "kingspades.png", "queenclubs.png", "queendiamonds.png", "queenhearts.png", "queenspades.png"]
+values = ["2","2","2","2","3","3","3","3","4","4","4","4","5","5","5","5","6","6","6","6","7","7","7","7","8","8","8","8","9","9","9","9","10","10","10","10"]
+values2 = ["11","11","11","11","10","10","10","10","10","10","10","10","10","10","10","10","10",]
 
-var num2_card = ["2clubs.png", "2diamonds.png", "2hearts.png", "2spades.png"]
-var num3_card = ["3clubs.png", "3diamonds.png", "3hearts.png", "3spades.png"]
-var num4_card = ["4clubs.png", "4diamonds.png", "4hearts.png", "4spades.png"]
-var num5_card = ["5clubs.png", "5diamonds.png", "5hearts.png", "5spades.png"]
-var num6_card = ["6clubs.png", "6diamonds.png", "6hearts.png", "6spades.png"]
-var num7_card = ["7clubs.png", "7diamonds.png", "7hearts.png", "7spades.png"]
-var num8_card = ["8clubs.png", "8diamonds.png", "8hearts.png", "8spades.png"]
-var num9_card = ["9clubs.png", "9diamonds.png", "9hearts.png", "9spades.png"]
-var num10_card = ["10clubs.png","10diamonds.png", "10hearts.png", "10spades.png"]
 
 
-function randomcards()
+
+
+function randomcards(sum)
 {
     random = Math.floor(Math.random() * 51)
+    random2 = Math.floor(Math.random() * 14)
+    if(ran == false)
+    {
 
-    console.log(random)
+        if(random >= 36)
+        {
+            
+            document.getElementById("player"+player).src = "img/"+face_cards[random2]
+            player++
+            value1 = values2[random2]
+            console.log(value1)
+            random = Math.floor(Math.random() * 51)
 
+
+
+            if(random >= 36)
+            {
+                random2 = Math.floor(Math.random() * 15)
+                document.getElementById("player"+player).src = "img/"+face_cards[random2]
+                value2 = values2[random2]
+                 sum = +value1 + +value2
+                 console.log(sum+", 1")
+                 hitMe(sum = +value1 + +value2)
+            }
+
+
+            else
+            {
+                document.getElementById("player"+player).src = "img/"+random+".png"
+                value2 = values[random]        
+                 sum =  +value1 + +value2
+                 console.log(sum+", 2")
+                 hitMe(sum = +value1 + +value2)
+                
+            }
+
+            document.getElementById("valueocards").innerHTML = sum
+            ran = true
+        }
+
+
+        else
+        {
+            document.getElementById("player"+player).src = "img/"+random+".png"
+            player++
+            value1 = values[random]
+            randomer = Math.floor(Math.random() * 51)
+
+            if(randomer >= 36)
+            {
+                randomer2 = Math.floor(Math.random() * 14)
+                document.getElementById("player"+player).src = "img/"+face_cards[randomer2]
+                value2 = values2[randomer2]
+               sum = +value1 + +value2
+               console.log(sum+", 3")
+               hitMe(sum = +value1 + +value2)
+            }
+
+            else
+            {
+
+                document.getElementById("player"+player).src = "img/"+randomer+".png"
+                value2 = values[randomer]
+                sum = +value1 + +value2
+                console.log(sum+", 4")
+                hitMe(sum = +value1 + +value2)
+                
+            }
+
+            document.getElementById("valueocards").innerHTML = sum
+            ran = true
+
+        }
+     
+    }// end of ran
+    
+variable1 = sum
+player++
 }
+
+function hitMe(variable1, local)
+{
+
+    if(local == true)
+    {
+    randomest = Math.floor(Math.random() * 51)
+    randomest2 = Math.floor(Math.random() * 14)
+
+        if(randomest >= 36)
+        {
+        console.log(variable1)
+        document.getElementById("player"+player).src = "img/"+face_cards[randomest2]
+        sumation = +variable1 + +values[randomest2]
+        console.log(sumation)
+        if(sumation > 21)
+        {
+            document.getElementById("valueocards").innerHTML = "you busted!"
+        }            
+        }
+        else
+        {
+            console.log(variable1+" notfacecard")
+            console.log(player)
+            document.getElementById("player"+player).src = "img/"+randomest+".png"        
+            sumation = +variable1 + +values[randomest]
+            player++
+
+            if(sumation > 21)
+            {
+                document.getElementById("valueocards").innerHTML = sumation
+                console.log("lol")
+            }   
+            else{
+                console.log(player)
+                document.getElementById("valueocards").innerHTML = sumation
+                player++
+            }      
+        }// end of else statement
+
+
+    }
+
+    
+}
+
+
